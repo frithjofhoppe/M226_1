c3c63e53e209ae2430c1d3832096737628d356ab
kruettlip <philippe.kruettli@gmx.ch>
2017-08-31 16:08:48 +0200

    Added lesson 1

---
+++ M226_1/src/hz_3/Competence_1_1.java
@@ -0,0 +1,20 @@
+package hz_3;
+
+public class Competence_1_1 {
+
+	public static void main(String[] args) {
+		int numberOfStudents = 21;
+		boolean isLightSwitchOn = true;
+		double saldoOfBankAccount = 6723.93;
+		char nameOfChessLine = 'C';
+		
+		numberOfStudents += 2;
+		isLightSwitchOn = false;
+		saldoOfBankAccount = 45621.12;
+		nameOfChessLine = 'F';
+		
+		System.out.println("Number of students: " + numberOfStudents + "\nIs light switch on? " + isLightSwitchOn + 
+				"\nBank account saldo: " + saldoOfBankAccount + "\ncurrent chess line name: " + nameOfChessLine);
+	}
+
+}
