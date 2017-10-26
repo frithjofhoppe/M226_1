d2f40855adfec6f78f1bffe2d03db21075a76962
kruettlip <philippe.kruettli@gmx.ch>
2017-10-26 15:38:18 +0200

    testPackage updated

--- M226_1/src/testPackage/Main.java
+++ M226_1/src/testPackage/Main.java
@@ -7,12 +7,17 @@ public class Main
 	{
 		Person p = new Person("Thomas");
 		Person q = new Person("Maria");
+		Person y = p;
 		Person[] pArray = new Person[3];
 		pArray[0] = p;
+		pArray[1] = y;
 		pArray[2] = q;
-		p = new Person("Josef");
+		//p = new Person("Josef");
+		p.name = "Josef";
 		System.out.println(pArray[0].name);
+		System.out.println(pArray[1].name);
 		System.out.println(p.name);
+		System.out.println(y.name);
 	}
 
 }
