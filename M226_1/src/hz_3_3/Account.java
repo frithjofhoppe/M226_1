d44cd04c89a8c6c6a190716f76e99d834a0cc84a
frithjofhoppe <frithjof.hoppe@icloud.com>
2017-11-02 12:41:13 +0100

    made some changes

--- M226_1/src/hz_3_3/Account.java
+++ M226_1/src/hz_3_3/Account.java
@@ -1,27 +1,80 @@
 package hz_3_3;
 
+/**
+ * A bank account used to place money
+ * @author Philippe Krüttli
+ *
+ */
 public class Account
 {
-	private double saldo = 0;
-	private double zinssatz = 1;
+	private double balance = 0; // Current amount of money on the account
+	private double interestRate = 1; // The percentage value of the interest
 	
-	Account(double saldo)
+	/**
+	 * Create a new account and set its balance
+	 * @param balance: the balance to set initially for the account
+	 */
+	Account(double balance)
 	{
-		this.saldo = saldo;
+		this.balance = balance;
 	}
 	
-	public double getSaldo() {
-		return saldo;
-	}
-
-	public void einzahlen(double wert)
+	/**
+	 * Create a new account and set its balance and interest rate
+	 * @param balance: the balance to set initially for the account
+	 * @param interestRate: the interest rate to set initially for the account
+	 */
+	Account(double balance, double interestRate)
 	{
-		saldo+=wert;
+		this.balance = balance;
+		this.interestRate = interestRate;
 	}
 	
-	public void verzinsen(double tag)
+	/**
+	 * Get the current balance of the account
+	 * @return: current balance
+	 */
+	public double getBalance() {
+		return balance;
+	}
+	
+	/**
+	 * Get the current interest rate of the account
+	 * @return: interest rate
+	 */
+	public double getInterestRate() {
+		return interestRate;
+	}
+	
+	/**
+	 * Deposit a specified amount of money on the account
+	 * @param value: amount of money to deposit on the account
+	 */
+	public void deposit(double value)
 	{
-		double zins = saldo * zinssatz * (tag/365);
-		saldo += zins;
+		balance+=value;
+	}
+	
+	/**
+	 * Withdraw a specified amount of money from the account
+	 * @param value: amount of money to withdraw from the account
+	 */
+	public void withdrawMoney(double value)
+	{
+		if(value <= balance)
+		{
+			balance -= value;
+		}
+	}
+	
+	/**
+	 * Pay interest on that account for a specified amount of days
+	 * Calculating like: balance * interest rate * (days/365 days)
+	 * @param days: amount of days to pay interest for
+	 */
+	public void payInterest(double days)
+	{
+		double interest = balance * interestRate * (days/365);
+		balance += interest;
 	}
 }
--- M226_1/src/hz_3_3/TestAccount.java
+++ M226_1/src/hz_3_3/TestAccount.java
@@ -1,18 +1,24 @@
 package hz_3_3;
 
+/**
+ * A test class to test the functionality of the Account-class
+ * @author Philippe Krüttli
+ *
+ */
 public class TestAccount
 {
 
 	public static void main(String[] args)
 	{
-		Account k1 = new Account(500.75);
-		k1.verzinsen(365);
+		Account a1 = new Account(500.75); // Create new account with balance 500.75
+		Account a2 = new Account(720.50); // Create new account with balance 720.50
 		
-		Account k2 = new Account(720.50);
-		k2.verzinsen(750);
-		System.out.println(k2.getSaldo());
-		k2.einzahlen(200);
-		System.out.println(k2.getSaldo());
+		a1.payInterest(365); // Pay interest for 1 year
+		a2.payInterest(750); // Pay interest for 750 days
+		System.out.println(a2.getBalance()); // Print out current balance of account a2
+		
+		a2.deposit(200); // Deposit value of 200 on account a2
+		System.out.println(a2.getBalance()); // Print out current balance of account a2
 	}
 
 }
