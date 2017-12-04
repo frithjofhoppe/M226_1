d601af81c94c248acef8ea15ecb9bdb780f0c97c
kruettlip <philippe.kruettli@gmx.ch>
2017-12-04 20:13:56 +0100

    removed unused imports

--- M226_1/src/Minesweeper/PlayingField.java
+++ M226_1/src/Minesweeper/PlayingField.java
@@ -6,8 +6,6 @@ import javafx.event.EventHandler;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
